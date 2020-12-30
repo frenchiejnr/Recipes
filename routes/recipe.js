bb9173c1e650124d7d4741c8de3f95ec2f3577fd
binyomin.freedman <binyomin.freedman@digitalbridge.eu>
2020-12-30 17:43:47 +0000

    Read file

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.use(function (req, res) {
-  console.log("recipe");
   let filename = req.originalUrl;
   console.log(filename);
   filename = filename.split('_')[1];
   filename = `recipes/${filename}.md`;
   console.log(filename)
+
+  let recipe = fs.readFileSync(filename, 'utf-8');
+  // recipe = md.makeHtml(recipe);
+  console.log(recipe);
   })
 
 
